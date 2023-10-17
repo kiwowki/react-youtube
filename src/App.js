46c13580561ba19b4c43061039c14ac744739561
kiwowki <wolves941110@gmail.com>
2023-10-17 14:21:27 +0900

    깃페이지정리

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Today from './pages/Today'
+import Musician from './pages/Musician'
+import Not from './pages/Not'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/today' element={<Today />} />
+                <Route path='/musician' element={<Musician />} />
+                <Route path='*' element={<Not />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
